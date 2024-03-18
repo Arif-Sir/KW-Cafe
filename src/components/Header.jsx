f48fb3a0e787281da23e7a6cdfc1536e34447934
Arif-Sir <arifrahman9122@gmail.com>
2024-03-19 02:36:16 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+           <h1 className="text-3xl bg-blue-300 font-bold "> KW-Cafe</h1> 
+        </div>
+    );
+};
+
+export default Header;
